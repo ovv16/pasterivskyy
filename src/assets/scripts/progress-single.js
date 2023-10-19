50962da95f0d04829e512cd6647ac36c6d80a48f
yatsinoa <sashayatzino@gmail.com>
2023-10-19 15:09:46 +0300

    swiper changes

--- src/assets/scripts/progress-single.js
+++ src/assets/scripts/progress-single.js
@@ -1,23 +1,22 @@
 import Swiper, { Navigation } from 'swiper';
 Swiper.use([Navigation]);
 
-
-var swiper = new Swiper(".swiper-gallery", {
-	speed: 1500,
-	slidesPerView: 1,
-	spaceBetween: 14,
-	breakpoints: {
-		768: {
-			slidesPerView: 1.5,
-		},
-		1366: {
-			spaceBetween: 20,
-		},
-	},
-	navigation: {
-		nextEl: ".swiper-navigation__arow-right",
-		prevEl: ".swiper-navigation__arow-left",
-	},
-	mousewheel: true,
-	keyboard: true,
+var swiper = new Swiper('.swiper-gallery', {
+  speed: 1500,
+  slidesPerView: 'auto',
+  spaceBetween: 14,
+  breakpoints: {
+    768: {
+      slidesPerView: 1.5,
+    },
+    1366: {
+      spaceBetween: 20,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-navigation__arow-right',
+    prevEl: '.swiper-navigation__arow-left',
+  },
+  mousewheel: true,
+  keyboard: true,
 });
